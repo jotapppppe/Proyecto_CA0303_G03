ec2f5a871c21b794329ccb7184150d183f385fc0
Juan Pablo Morgan <juanpablomorgans@gmail.com>
2024-06-26 23:40:09 -0600

    feat: se suben el setup para el proyecto

---
+++ cod/setup.R
@@ -0,0 +1,17 @@
+# Librerías utilizadas
+pacman::p_load(
+  rmdformats,
+  tidyverse,
+  tidyr,
+  dplyr,
+  readxl,
+  readr,
+  ggplot2,
+  stats,
+  corrplot,
+  confintr,
+  nortest,
+  plotly
+)
+
+source("cod/lectura_de_datos.R")
